ba50c18a41f3420501666c6b792151beee03b949
Zakant <zakant@gmx.de>
2014-08-07 18:23:25 +0200

    Fixed bugs in ColorChanger.cs

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -20,13 +20,22 @@ public class ColorChanger : MonoBehaviour
     {
         GUILayout.BeginArea(new Rect(Screen.width - 100, 20, 90, Screen.height - 25));
         GUILayout.Label("R:");
-        var r = GUILayout.HorizontalSlider(material.color.r, 0, 255, null);
+        var r = GUILayout.HorizontalSlider(material.color.r, 0, 1, null);
         GUILayout.Label("G:");
-        var g = GUILayout.HorizontalSlider(material.color.g, 0, 255, null);
+        var g = GUILayout.HorizontalSlider(material.color.g, 0, 1, null);
         GUILayout.Label("B:");
-        var b = GUILayout.HorizontalSlider(material.color.b, 0, 255, null);
-        GUILayout.Label("");
-        GUILayout.Label(string.Format("R:{0} G:{1} B:{2}", r, g, b));
+        var b = GUILayout.HorizontalSlider(material.color.b, 0, 1, null);
+        GUILayout.Space(20);
+
+        Texture2D t = new Texture2D(1, 1);
+        t.SetPixel(0, 0, new Color(r, g, b));
+        t.wrapMode = TextureWrapMode.Repeat;
+        t.Apply();
+        GUIStyle s = new GUIStyle();
+        s.normal.background = t;
+        GUILayout.Label("", s);
+
+
         GUILayout.EndArea();
         material.color = new Color(r, g, b);
     }
