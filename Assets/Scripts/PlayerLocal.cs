7773d28e441baffcd847698476e8cd3e557715b6
Zakant <zakant@gmx.de>
2014-08-12 16:37:06 +0200

    Added Code from http://wiki.unity3d.com/index.php?title=NetworkView_Position_Sync

--- Assets/Scripts/PlayerLocal.cs
+++ Assets/Scripts/PlayerLocal.cs
@@ -3,24 +3,22 @@ using System.Collections;
 
 public class PlayerLocal : MonoBehaviour
 {
+    private Vector3 p;
+    private Quaternion r;
+    private int m = 0;
 
-    // Use this for initialization
     void Start()
     {
-
+        networkView.observed = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnSerializeNetworkView(BitStream stream)
     {
-
+        p = rigidbody.position;
+        r = rigidbody.rotation;
+        m = 0;
+        stream.Serialize(ref p);
+        stream.Serialize(ref r);
+        stream.Serialize(ref m);
     }
-
-
-
-    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
-    {
-
-    }
-
 }
