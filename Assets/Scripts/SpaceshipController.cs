5644fb8398ba1694944bd9d2658f385eb034a4c5
Zakant <zakant@gmx.de>
2014-09-14 09:06:17 +0200

    Fixed Typo

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class SpaceshipConroller : MonoBehaviour
+public class SpaceshipController : MonoBehaviour
 {
 	float geschwindigkeit;
 	public int standartGeschwindigkeit;
